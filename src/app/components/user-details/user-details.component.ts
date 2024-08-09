017d277f973f7c411125482a0b445fce07d4c3ed
Ahmed Galal <ahmedgalalmohamed912@gmail.com>
2024-08-09 14:47:50 +0300

    refactor: Update user details component to use NgRx store for user data retrieval

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,32 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { UserService } from '../../services/user.service';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+import {
+  selectOnlyUser,
+  selectUserStatus,
+} from '../../state/selectors/user.selector';
+import { select, Store } from '@ngrx/store';
+import { AppState } from '../../state/app.state';
+import { loadUser } from '../../state/actions/user.action';
+import { User } from '../../state/reducers/user.reducer';
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   standalone: true,
   styleUrl: './user-details.component.css',
-  imports: [HttpClientModule, CommonModule],
-  providers: [UserService],
+  imports: [CommonModule , MatProgressSpinnerModule],
 })
 export class UserDetailsComponent implements OnInit {
-  user: any;
+  user$: Observable<User | null> = this.store.pipe(select(selectOnlyUser));
+  loading$: Observable<boolean> = this.store.pipe(
+    select(selectUserStatus),
+    map((status) => status === 'loading')
+  );
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private userService: UserService
+    private store: Store<AppState>
   ) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe((params) => {
       const userId = params.get('id');
       if (userId) {
-        this.userService.getUserById(Number(userId)).subscribe((data) => {
-          this.user = data.data;
-        });
+        this.store.dispatch(loadUser({ id: Number(userId) }));
       }
     });
   }
