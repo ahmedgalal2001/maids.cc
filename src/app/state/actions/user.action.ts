ca73fd40306075e8666d0dafe017a7261c0bed5f
Ahmed Galal <ahmedgalalmohamed912@gmail.com>
2024-08-09 12:44:53 +0300

    feat: Add user actions for loading users and user details

---
+++ src/app/state/actions/user.action.ts
@@ -0,0 +1,28 @@
+import { createAction, props } from '@ngrx/store';
+
+export const loadUsers = createAction(
+  '[User] Load Users',
+  props<{ page: number }>()
+);
+export const loadUsersSuccess = createAction(
+  '[User] Load Users Success',
+  props<{ users: any[]; total_pages: number }>()
+);
+export const loadUsersFailure = createAction(
+  '[User] Load Users Failure',
+  props<{ error: any }>()
+);
+
+export const loadUser = createAction(
+  '[User] Load User',
+  props<{ id: number }>()
+);
+export const loadUserSuccess = createAction(
+  '[User] Load User Success',
+  props<{ user: any }>()
+);
+
+export const loadUserFailure = createAction(
+  '[User] Load User Failure',
+  props<{ error: any }>()
+);
